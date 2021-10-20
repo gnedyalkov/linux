ee47111f549d571f2024dec0386e029db440d383
GNedyalk0v <gnedyalk0v@protonmail.ch>
2021-10-20 17:56:35 +0300

    test

--- initial.sh
+++ initial.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+# Disabling IPv6 on Kernel Level
+# Make a copy of the grub config file first
+cp /etc/default/grub /etc/default/grub.bak
+
+# Add ipv6.disable=1 to grub's config file
+sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="/&ipv6.disable=1 /' /etc/default/grub
+
+# Update the grub
+update-grub2
+
+# Update our system
+apt update
+apt upgrade -y
+apt autoremove -y
