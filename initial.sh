3abfc7424cbd3f6727b13783fce87d251b319dd5
GNedyalk0v <gnedyalk0v@protonmail.ch>
2021-10-20 18:43:57 +0300

    test2

--- initial.sh
+++ initial.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+### EDIT THESE VARIABLES BASED ON YOUR NEEDS ###
+server_hostname="server.hostname.here"
+server_timezone="Europe/London"
+server_nameserver1="9.9.9.9"
+server_nameserver2="1.1.1.1"
+server_swapfile_size="4G"
+
+# Set hostname
+hostnamectl set-hostname $server_hostname
+
+# Set timezone
+timedatectl set-timezone $server_timezone
+
 # Disabling IPv6 on Kernel Level
 # Make a copy of the grub config file first
 cp /etc/default/grub /etc/default/grub.bak
 
 # Add ipv6.disable=1 to grub's config file
-sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="/&ipv6.disable=1 /' /etc/default/grub
+sed -i 's/^GRUB_CMDLINE_LINUX="/&ipv6.disable=1 /' /etc/default/grub
 
 # Update the grub
 update-grub2
@@ -14,3 +27,21 @@ update-grub2
 apt update
 apt upgrade -y
 apt autoremove -y
+
+# Install some useful tools
+apt install -y unattended-upgrades update-notifier-common net-tools vim wget
+
+# Set swap file
+fallocate -l $server_swapfile_size /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+
+# Make sure SWAP is loaded on boot
+cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab
+
+# Optimize SWAP usage
+cp /etc/sysctl.conf /etc/sysctl.conf.bak
+echo 'vm.swappiness=10' | tee -a /etc/sysctl.conf
+echo 'vm.vfs_cache_pressure = 50' | tee -a /etc/sysctl.conf
